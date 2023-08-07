e49600fb766aab29f3f466b7004074c203050fe9
MaRui <bing6749@163.com>
2023-08-07 22:45:18 +0800

    初始化项目

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/*
+ * @author  MaRui
+ * @date  2023/8/7 22:19
+ * @version 1.0
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/top/bing6749/BinarySearch.java
@@ -0,0 +1,16 @@
+package top.bing6749;
+/*
+ * @author  MaRui
+ * @date  2023/8/7 22:23
+ * @version 1.0
+ */
+
+
+/**
+ * @author MaRui
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
